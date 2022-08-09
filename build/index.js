4034d6b43495a4c788ac698498cea0ecadfff3ab
Murilo <murilo@astrusweb.com>
2022-08-08 21:19:26 -0300

    TypeScript basic Curso

---
+++ build/index.js
@@ -0,0 +1,46 @@
+"use strict";
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+var listaSalva = localStorage.getItem("@listagem_tarefas");
+var tarefas = listaSalva !== null && JSON.parse(listaSalva) || [];
+function listarTarefas() {
+    listElement.innerHTML = "";
+    tarefas.map(function (item) {
+        var todoElement = document.createElement("li");
+        var tarefaText = document.createTextNode(item);
+        var linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+        var posicao = tarefas.indexOf(item);
+        linkElement.setAttribute("onclick", "deletarTarefa(".concat(posicao, ")"));
+        linkElement.setAttribute("style", "margin-left:10px;");
+        var linkText = document.createTextNode(" Excluir");
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(tarefaText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    });
+}
+listarTarefas();
+function adicionarTarefa() {
+    if (inputElement.value === "") {
+        alert("Digite alguma tarefa!");
+        return false;
+    }
+    else {
+        var tarefaDigitada = inputElement.value;
+        tarefas.push(tarefaDigitada);
+        inputElement.value = "";
+        listarTarefas();
+        salvarDados();
+    }
+}
+buttonElement.onclick = adicionarTarefa;
+function salvarDados() {
+    localStorage.setItem("@listagem_tarefas", JSON.stringify(tarefas));
+}
+function deletarTarefa(posicao) {
+    tarefas.splice(posicao, 1);
+    listarTarefas();
+    salvarDados();
+}
